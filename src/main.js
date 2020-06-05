952406109cf5ce5cb2a79cbea323405c5b23e997
Enrique <enrique.delgado@live.com>
2020-06-04 21:06:45 -0500

    Make axios global

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Vuex from "vuex";
+import Axios from "axios";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
+Vue.prototype.$http = Axios;
 
 new Vue({
   render: (h) => h(App),
