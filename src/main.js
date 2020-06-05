9105225290bfaa0172230f36b1abcae995f36253
Enrique <enrique.delgado@live.com>
2020-06-04 21:19:13 -0500

    Add vue-material

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vuex from "vuex";
 import Axios from "axios";
+import "./shared/material/material";
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
---
+++ src/shared/material/material.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import VueMaterial from "vue-material";
+import { MdButton } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(VueMaterial);
+Vue.use(MdButton);
