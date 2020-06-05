bf0cf5b06995cc543cba0de57e1831a41ea66161
Enrique <enrique.delgado@live.com>
2020-06-05 12:38:23 -0500

    Add a theme

--- src/shared/material/material.js
+++ src/shared/material/material.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueMaterial from "vue-material";
 import { MdButton } from "vue-material/dist/components";
 import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
 Vue.use(VueMaterial);
 Vue.use(MdButton);
